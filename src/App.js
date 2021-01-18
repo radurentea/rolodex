8b2e42c283ea71340c71aa24be68decb32c29d0d
Radu Rentea <radu.rentea@gmail.com>
2021-01-18 21:13:49 +0200

    Change searchBox into a component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {CardList} from './components/card-list/card-list.component'
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends React.Component {
@@ -25,7 +26,10 @@ class App extends React.Component {
     })
     return (
       <div className="App">
-        <input type= 'search' placeholder='Search players' onChange={ e => this.setState({ searchField: e.target.value }) }/>
+        <SearchBox
+          placeholder='Search players'
+          handleChange={ e => this.setState({ searchField: e.target.value }) }
+        />
         <CardList players={ filteredPlayers }/>
       </div>
     );
